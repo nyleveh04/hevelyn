bda29ea7911d274f5dbfd4355893d5f812d5a3d6
nyleveh04 <106618965+nyleveh04@users.noreply.github.com>
2024-03-25 14:26:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes)
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+const contadores = document.querySelectorAll(".contador");
+contadores[0].textContent = "Contagem regressiva";
+
+
 
